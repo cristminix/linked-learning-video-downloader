1ac56e529f726f7401473baeafdb389d3451dfd9
Putra Budiman <cristminix@gmail.com>
2022-01-29 07:38:43 +0700

    add utility to rename files

--- server/rename_downloaded_file.py
+++ server/rename_downloaded_file.py
@@ -3,12 +3,13 @@
 import os
 import json
 
-folder 	= r'C:\Users\Damar\Downloads\Video\python-for-non-programmer'
+folder 	= r'C:\Users\Damar\Downloads\Video\python-for-non-programmer\\'
 
 count 	= 1
 
-session_json_path = "storage/session.json"
-session = {}
+session_json_path 	= "storage/session.json"
+session 			= {}
+files_to_rename 	= {}
 
 def init_session_db():
     global session
@@ -16,44 +17,51 @@ def init_session_db():
         session = json.load(json_file)
 
 def query_filenames():
+	global files_to_rename
+
 	for sessionId in session:
 		sessionData = session[sessionId]
 		tocs 	= sessionData['tocs']
-		index = 0;
+		index   = 1;
 		for item in tocs:
 			# print(item)
 			slug 			= item['slug']
 			videoUrl 		= item['videoUrl']
 			videoUrlSplit	= videoUrl.split('/')
 			captionUrl 		= item['captionUrl']
+			
 			videoFilename 	= videoUrlSplit[len(videoUrlSplit)-1].split('?')[0]+'.mp4'
 			captionFilename	= '' 
-			if index > 0 :
-				captionFilename = 'ambry' + '_' + str(index) 
-			else:
-				captionFilename = 'ambry'
+
+			# if index > 0 :
+			captionFilename = 'ambry' + '_' + str(index) 
+			# else:
+			# 	captionFilename = 'ambry'
 
 			captionFilename += '.html'	
 			index += 1
-			print('%s|%s|%s' %(slug,videoFilename,captionFilename))
-		# print(courseInfo)
+			
+			newVideoFilename 	= slug + '.mp4'
+			newCaptionFilename	= slug + '.vtt'
+
+			# print('%s|%s ==> %s|%s' %(videoFilename, captionFilename, newVideoFilename, newCaptionFilename))
+			files_to_rename[videoFilename]		=	newVideoFilename
+			files_to_rename[captionFilename]	=	newCaptionFilename
+
+def do_rename_files():
+	global folder
+	global files_to_rename
+
+	for oldFilename in files_to_rename:
+	    srcFilename = folder + oldFilename
+	    dstFilename = folder + files_to_rename[oldFilename]
+	    if os.path.exists(srcFilename) :
+	    	print('Rename : %s to %s' % (oldFilename, files_to_rename[oldFilename]))
+	    	os.rename(srcFilename, dstFilename)
+	    	
+	    else:
+	    	print('Skip   : %s' % (oldFilename))
 
 init_session_db()
 query_filenames()
-
-# count increase by 1 in each iteration
-# iterate all files from a directory
-for file_name in os.listdir(folder):
-    # Construct old file name
-    source = folder + file_name
-    print(file_name)
-    # Adding the count to the new file name and extension
-    # destination = folder + "sales_" + str(count) + ".txt"
-
-    # Renaming the file
-    # os.rename(source, destination)
-    count += 1
-# print('All Files Renamed')
-
-# print('New Names are')
-# verify the result
+do_rename_files()
